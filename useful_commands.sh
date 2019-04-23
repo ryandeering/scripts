4f7ee014ed8c03e948935ae280d4daf542740482
Ryan Deering <37181720+ryandeering@users.noreply.github.com>
2019-04-23 18:14:18 +0100

    Update useful_commands.sh

--- useful_commands.sh
+++ useful_commands.sh
@@ -11,3 +11,7 @@ acpi -t
 
 sudo du -a / | sort -n -r | head -n 20 && beep
 #biggest directories in file system
+
+sudo strace -p 1205 
+#See syscalls of PID 1205. Processes don't have to be a black box, you can use strace (on Linux) to view the system calls made by a process
+#which may give some clue as to why it's misbehaving, where it is saving a file, etc. here it is saving a file, etc.
